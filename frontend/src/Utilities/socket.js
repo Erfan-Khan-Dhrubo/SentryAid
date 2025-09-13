c8122b5192f8d6819a466e2688a1d9fe9571d82a
Erfan Khan <erfan.khan.cse@gmail.com>
2025-09-13 21:29:53 +0600

    Update socket.js

--- frontend/src/Utilities/socket.js
+++ frontend/src/Utilities/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5001");
+const socket = io("https://sentryaid-backend.onrender.com"); // https://sentryaid-backend.onrender.com
 export default socket;
